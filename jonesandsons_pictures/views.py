d154e060473b20a953972656dc745f9697e3f350
cjones1047 <cjmj1047@gmail.com>
2022-09-23 10:50:06 -0500

    Added function to process requests from React front-end so that response includes all pictures in database

--- jonesandsons_pictures/views.py
+++ jonesandsons_pictures/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+
+from jonesandsons_pictures.models import Pictures
+from jonesandsons_pictures.serializers import PicturesSerializer
 
 # Create your views here.
 def index(request):
     return HttpResponse("Hello world.")
+
+@csrf_exempt
+def picturesApi(request,id=0):
+    if request.method=="GET":
+        pictures = Pictures.objects.all()
+        pictures_serializer = PicturesSerializer(pictures, many=True)
+        return JsonResponse(pictures_serializer.data, safe=False)
